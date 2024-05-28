c32d1660702b97c3ff6124c489caa51fc06bb3a8
mochacino <lanaksa28@gmail.com>
2024-05-28 21:45:12 +0700

    init

---
+++ app/Providers/UserServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\UserService;
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+
+    public function provides()
+    {
+        return [UserService::class];
+
+    }
+
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
